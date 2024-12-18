68770b4fd5011a3700a36db28b67f20746a208d1
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2024-12-18 07:49:29 +0100

    add DSU solution

--- day18.py
+++ day18.py
@@ -3,6 +3,7 @@ from collections import Counter, defaultdict
 import hashlib
 import re
 
+adj4 = [(-1,0),(0,1),(1,0),(0,-1)]
 # trans={"U":(-1,0),"L":(0,-1), "D":(1,0),"R":(0,1)}
 def nums(line):
     return list(map(int,re.findall(r'-?\d+', line)))
@@ -20,7 +21,7 @@ def part1(data):
     seen={(0,0)}
     for steps,a, b in q:
         if (a,b) == (M-1,M-1): return steps
-        for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
+        for dr, dc in adj4:
             if 0<=a+dr<M and 0<=b+dc<M and (a+dr,b+dc) not in seen and (a+dr,b+dc) not in blocked:
                 seen.add((a+dr,b+dc))
                 q.append((steps+1,a+dr,b+dc))
@@ -30,20 +31,39 @@ def part1(data):
 
 def part2(data):
     M = 71
-    blocked=set()
+    blocked=[]
     for line in data.split("\n"):
-        blocked.add(tuple(nums(line)))
+        blocked.append(tuple(nums(line)))
+    Parent=list(range(M*M))
     
-        q=[(0,0,0)]
-        seen={(0,0)}
-        for steps,a, b in q:
-            if (a,b) == (M-1,M-1): break
-            for dr, dc in ((1,0),(-1,0),(0,1),(0,-1)):
-                if 0<=a+dr<M and 0<=b+dc<M and (a+dr,b+dc) not in seen and (a+dr,b+dc) not in blocked:
-                    seen.add((a+dr,b+dc))
-                    q.append((steps+1,a+dr,b+dc))
-        else:
-            return nums(line)
+    def find(x):
+        if Parent[x]!=x:
+            return find(Parent[x])
+        else: return x
+    
+    def union(x,y):
+        Parent[find(y)]=find(x)
+    
+    for r in range(M):
+        for c in range(M):
+            if (r,c) in blocked: continue
+            for dr, dc in ((-1,0),(0,-1)):
+                nr=r+dr
+                nc=c+dc
+                if 0<=nr<M and 0<=nc<M and (nr,nc) not in blocked:
+                    union(r*M+c,nr*M+nc)
+    blocked_set=set(blocked)
+    while blocked:
+        r,c=blocked.pop()
+        blocked_set.remove((r,c))
+        for dr, dc in adj4:
+            nr = r+dr
+            nc = c+dc
+            if 0<=nr<M and 0<=nc<M and (nr,nc) not in blocked_set:
+                union(nr*M+nc,r*M+c)
+        if find(Parent[0])==find(Parent[-1]):
+            return f"{r},{c}"
+    
 
 
 if __name__ == "__main__":
