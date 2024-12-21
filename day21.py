522efe1acf1e8500a55fd5d812f2985b5ef3dfcd
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2024-12-22 00:46:01 +0100

    haha

--- day21.py
+++ day21.py
@@ -16,65 +16,65 @@ def nums(line):
     return list(map(int,re.findall(r'-?\d+', line)))
 
 def part1(data):
-        total=0
-        grid1 = tuple(map(tuple,[["7","8","9"],["4","5","6"],["1","2","3"],["b","0","A"]]))
-        grid2= tuple(map(tuple,[["b","^","A"],["<","v",">"]]))
+    grid1=["789","456","123","B0A"]
+    grid2=["B^A","<v>"]
+    trans={(-1,0):"^",(1,0):"v",(0,1):">",(0,-1):"<"}
+    def bfs(start, end,grid):
+        if start == end: return [""]
+        M,N = len(grid), len(grid[0])
+        sr=sc=tr=tc=None
+        for r in range(M):
+            for c in range(N):
+                if grid[r][c]==start:
+                    sr,sc = r,c
+                if grid[r][c]==end:
+                    tr,tc=r,c
+        q=[("",sr,sc)]
+        res=[]
+        for word, r, c in q:
+            if (r,c)==(tr,tc):
+                break
+            for dr, dc in adj4:
+                if 0<=r+dr<M and 0<=c+dc<N and grid[r+dr][c+dc]!="B":
+                    q.append((word+trans[(dr,dc)],r+dr,c+dc))
+                if (r+dr,c+dc)==(tr,tc):
+                    res.append(word+trans[(dr,dc)])
+        return res
 
-        @cache
-        def seq(start, end,grid):
-            if start==end: return [[["A","A"]]]
-            N,M = len(grid[0]),len(grid)
-            sr=sc=tr=tc=None
-            for r in range(M):
-                for c in range(N):
-                    if grid[r][c]==start:
-                        sr, sc = r,c
-                    if grid[r][c]==end:
-                        tr,tc = r,c
-            q=[("",sr,sc)]
-            seen=set(q)
-            ret=[]
-            for word, r, c in q:
-                if (tr,tc)==(r,c):
-                    break
-                for dr, dc in adj4:
-                    if (tr,tc) == (r+dr,c+dc):
-                        ret.append(word+trans[(dr,dc)])
-                    if 0<=r+dr<M and 0<=c+dc<N and grid[r+dr][c+dc]!="b" and (word+trans[(dr,dc)],r+dr,c+dc) not in seen:
-                        seen.add((word+trans[(dr,dc)],r+dr,c+dc))
-                        q.append((word+trans[(dr,dc)],r+dr,c+dc))
-            ret=[list(map(list,zip("A"+row,row))) for row in ret]
-            m = min(map(len,ret))
-            return [a for a in ret if len(a)==m]
+    @cache
+    def dfs(start,end):
+        return bfs(start,end,grid2)
 
-        @cache
-        def dfs(depth, start, end):
-            if depth ==0: return 0
-            res=float("inf")
-            for child in seq(start,end,grid2):
-                cur = 0
-                for a, b in child:
-                    cur+=1+dfs(depth-1,a,b)
-                res=min(res,cur)
-            return res
+    total=0
+    for line in data.split("\n"):
+        chunks=[bfs(start,end,grid1) for start, end in zip("A"+line,line)]
+        alignments=[]
+        for l in chunks[0]:
+            for m in chunks[1]:
+                for n in chunks[2]:
+                    for k in chunks[3]:
+                        alignments.append(l+"A"+m+"A"+n+"A"+k+"A")
+        
+        miniminsofar=float("inf")
+        for alignment in alignments:
+            foofoo=0
+            for start, end in zip("A"+alignment,alignment):
+                first_level = [n+"A" for n in dfs(start,end)]
+                minsofar=float("inf")
+                for alignment in first_level:
+                    foo=0
+                    for start, end in zip("A"+alignment,alignment):
+                        foo+=min(len(n+"A") for n in dfs(start,end))
+                    minsofar=min(foo,minsofar)
+                foofoo+=minsofar
+            miniminsofar=min(miniminsofar,foofoo)
+        total+=miniminsofar*int(line[:-1])
 
-        for line in data.split("\n"):
-            pairs = list(zip("A"+line,line))
-            possible_results = [seq(a,b,grid1) for a, b in pairs]
-            length=float("inf")
-            for a in possible_results[0]:
-                for b in possible_results[1]:
-                    for c in possible_results[2]:
-                        for d in possible_results[3]:
-                            b[0][0]=a[-1][1]
-                            c[0][0]=b[-1][1]
-                            d[0][0]=c[-1][1]
-                            print(a+b+c+d)
-                            pairs = a+b+c+d
-                            length=min(length,sum(dfs(2,*pair) for pair in pairs))
+    return total
+        
+        
             
-            total+= length*int(line[:-1])
-        return total
+        
 
 
 def part2(data):
