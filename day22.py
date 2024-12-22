e1c9b55aa1b4bb6cb7abbe6cf65dc2282843bae6
mapo924d <martin.podhaisky@mailbox.tu-dresden.de>
2024-12-22 11:39:53 +0100

    simplify step function

--- day22.py
+++ day22.py
@@ -1,5 +1,5 @@
 import time
-from collections import Counter, defaultdict
+from collections import Counter, defaultdict, deque
 import hashlib
 import re
 import math
@@ -13,19 +13,10 @@ def nums(line):
     return list(map(int,re.findall(r'-?\d+', line)))
 
 def step(n):
-    m=n
-    m*=64
-    m^=n
-    m%=16777216
-    n=m
-    m//=32
-    m^=n
-    m%=16777216
-    n=m
-    m*=2048
-    m^=n
-    m%=16777216
-    return m
+    n=((n<<6)^n) % 16777216
+    n= ((n << 5)^n) % 16777216
+    n= ((n>>11)^n) % 16777216
+    return n
 
 def part1(data):
     total=0
@@ -40,17 +31,11 @@ def part1(data):
 def part2(data):
     total=Counter()
     for line in data.split("\n"):
-        n=int(line)
-        q=[n%10]
-        seen=set()
-        for _ in range(2000):
-            n=step(n)
-            q.append(n%10)
-            if len(q)>=5:
-                seq = tuple(b-a for a, b in zip(q[-5:],q[-4:]))
-                if seq not in seen:
-                    seen.add(seq)
-                    total[seq]+=n%10
+        cur=Counter()
+        q=[int(line)]
+        for _ in range(2000): q.append(step(q[-1]))
+        for i in range(len(q)-1,3,-1): cur[tuple(q[j]%10-q[j-1]%10 for j in range(i-3,i+1))]=q[i]%10
+        total+=cur
     return max(total.values())
 
 if __name__ == "__main__":
